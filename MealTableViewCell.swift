2bb5356eb9df1db2b40a0dd50d94322ac4c09dde
Kohei Yamamoto <kymmt90@gmail.com>
2016-09-23 00:17:04 +0900

    Create a Table View

---
+++ MealTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kohei Yamamoto on 2016/09/22.
+//  Copyright © 2016年 Kohei Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratringControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
